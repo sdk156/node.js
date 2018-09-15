7d684263b6ad20efb93c46b35b15b8f80bb40c82
sdk156 <dineshkrishna1997@gmail.com>
2018-09-15 18:12:19 +0530

    Create noLoop_Callback.js

---
+++ assessment_1/noLoop_Callback.js
@@ -0,0 +1,14 @@
+//1.Print number 1 to 100 without using any loop. 
+
+// this program uses recursion and callback to print from 1 to 100 
+function noLoop(num,call){
+if(num==1){
+return call(num);
+}else{
+noLoop(num-1,call);
+return call(num);
+}
+    
+}
+function fun(num1){console.log(num1); }//callback function declaration
+noLoop(100,fun);// call
