424b9a6240c15d2fddd7102ae1ffb16ffa4de232
sdk156 <dineshkrishna1997@gmail.com>
2018-10-30 18:36:08 +0530

    Delete replaceWords.js

--- Assignment_3/replaceWords.js
+++
@@ -1,9 +0,0 @@
-var string1="hi Hi how are you hi who are you hi Hi hello hi Hi";
-console.log("initial string");
-console.log(string1);
-console.log("after replacemnt");
-var newString=string1.replace(/hi/g,"who");// case sensitive repacement
-console.log(newString);
-string1="hi Hi how are you hi who are you hi hi hello hi hi";
-var newString=string1.replace(/hi/gi,"");// case insensitive repacement
-console.log(newString);
