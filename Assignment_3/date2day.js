b96ff08509c90c85a5d98ef86b7a059fe22e2739
sdk156 <dineshkrishna1997@gmail.com>
2018-10-30 18:31:14 +0530

    Add files via upload

---
+++ Assignment_3/date2day.js
@@ -0,0 +1,30 @@
+var date1=new Date();
+var date2= new Date(2018,9,18,22,00,00);
+findDay(date1);
+findDay(date2);
+function findDay(d){
+    switch(d.getDay()){
+        case 0:
+        console.log("Sunday");
+        break;
+        case 1:
+        console.log("Monday");
+        break;
+        case 2:
+        console.log("Tuesday");
+        break;
+        case 3:
+        console.log("Wednesday");
+        break;
+        case 4:
+        console.log("Thursday");
+        break;
+        case 5:
+        console.log("Friday");
+        break;
+        case 6:
+        console.log("Saturday");
+        break;
+
+    }
+}
---
+++ Assignment_3/spliceMethod.js
@@ -0,0 +1,14 @@
+var games=["Temple Run","Subway Surfers","Fruit Ninja", "shadow Fight"];// an array
+console.log("initial array",games);
+var removedContent=games.splice(1,0,"PUBG");// 1 element inserted at index 1 without removal
+console.log("current Array",games);
+console.log("deleted items",removedContent);
+removedContent=games.splice(2,2,"solitaire","Minesweeper","COD");// removed 2 elements from index 2 and inserted 3 elements
+console.log("current Array",games);
+console.log("deleted items",removedContent);
+removedContent=games.splice(2,2);// removed 2 elements from index 2
+console.log("current Array",games);
+console.log("deleted items",removedContent);
+
+
+
