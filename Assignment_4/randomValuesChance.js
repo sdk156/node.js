5bf43f188f022b5e9300774d13756cbda8361ca4
sdk156 <dineshkrishna1997@gmail.com>
2018-10-31 23:53:32 +0530

    Add files via upload

--- Assignment_4/randomValuesChance.js
+++ Assignment_4/randomValuesChance.js
@@ -1,33 +1,27 @@
 //6. Demonstrate the use of chance, your system should always return a object where values for its keys are random.
 //Object should have at least these keys name, age, email, subjects, cgpa.
+// 6. Demonstrate the use of chance, your system should always return a object where values for its keys are random.
+// Object should have at least these keys name, age, email, subjects, cgpa.
 
 
 
-var ChancePackage=require('chance');
-var Chance= new ChancePackage();
-// function returnRandomObj(){
-//     let obj={
-//         //'name':Chance.string({pool:'abcdefghijklmnopqrstuvwxyz'}),
-//         'age':Chance.integer({min:17,max:30}),
-//         'email':Chance.email(),
-//         'subjects':Chance.unique(Chance.string({pool:'asdfghjkl'}),4),
-//         'CGPA':Chance.floating({min:0,max:10})
+var ChancePackage=require('chance'); //import chance module
+var Chance= new ChancePackage();// create an object to use its member functions
 
-//     }
-//     return obj;
+//function which returns random values for the required keys
+function returnRandomObj(){
+    let obj={
+        'name':Chance.string({pool:'abcdefghijklmnopqrstuvwxyz'}),
+        'age':Chance.integer({min:17,max:30}),
+        'email':Chance.email(),
+        'subjects':Chance.unique(Chance.string,4,{pool:'asdfghjkl',length:'10'}),
+        'CGPA':Chance.floating({min:0,max:10})
 
-// }
-// for(let i=1;i<5;i++){
+    }
+    return obj;
+
+}// this loop returns five random objects
+for(let i=1;i<5;i++){
     
-//     console.log(returnRandomObj());
-// }
-var p=Chance.string({pool:'abcdefghijklmnopqrstuvwxyz'});
-console.log(p);
-var obj={
-            'name':Chance.string({pool:'abcdefghijklmnopqrstuvwxyz'}),
-            'age':Chance.integer({min:17,max:30}),
-            'email':Chance.email(),
-            // 'subjects':Chance.unique(Chance.string({pool:'asdfghjkl'}),4),
-            'CGPA':Chance.floating({min:0,max:10})
-    
-        };
+    console.log(returnRandomObj());
+}
